47a0c4a6f493ad63c2b7ecae071f2f2797a5c2e3
Jay Tailor <tailorj64@gmail.com>
2019-04-13 14:32:58 +0530

    Added FAB

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import {View, Text, StyleSheet, Button, AsyncStorage, ActivityIndicator} from 'react-native'
 import {APP_NAME} from '../constants/strings'
 import {primary, primary_light} from '../constants/colors'
+import ActionButton from 'react-native-circular-action-menu';
+import Icon from "react-native-vector-icons/FontAwesome";
+import TaskListItem from '../components/TaskListItem'
 
 class Home extends Component {
 
@@ -20,6 +23,18 @@ class Home extends Component {
               title="I'm done, sign me out"
               onPress={this._signOutAsync}
           />
+
+          <View style={styles.fabContainer}>
+            <ActionButton buttonColor="black">
+              <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
+                <Icon name="sticky-note" style={styles.actionButtonIcon} />
+              </ActionButton.Item>
+              <ActionButton.Item buttonColor='#9b59b6' title="New Note" onPress={() => {}}>
+                <Icon name="tasks" style={styles.actionButtonIcon} />
+              </ActionButton.Item>
+            </ActionButton>
+
+          </View>
         </View>
     )
   }
@@ -32,5 +47,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  actionButtonIcon: {
+    fontSize: 20,
+    height: 22,
+    color: 'white',
+  },
+  fabContainer:{
+    flex:1,
+    width:100,
+    marginBottom:10
   }
 });
