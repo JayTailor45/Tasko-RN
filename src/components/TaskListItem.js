d09e6b526fd48eb517c9a713f005585dcd9916b9
Jay Tailor <tailorj64@gmail.com>
2019-04-13 15:08:33 +0530

    Refactored style

--- src/components/TaskListItem.js
+++ src/components/TaskListItem.js
@@ -1,25 +1,25 @@
 import React, {Component} from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
 
-class TaskListItem extends Component{
-  render(){
+class TaskListItem extends Component {
+  render() {
     return (
-        <View style={{ height: 100, width: '100%', flexDirection: 'row'}}>
+        <View style={styles.root}>
 
-          <View style={{ height: '100%',width: '4%', backgroundColor: 'red', marginEnd: 16}}/>
+          <View style={styles.priorityIndicator}/>
 
-          <View style={{ marginTop: 18,height: 50, width: '80%' }}>
+          <View style={styles.taskDataContainer}>
 
             <View>
-              <Text elipsis={'end'} style={{ fontWeight: 'bold', fontSize: 22, textTransform: 'capitalize'}}> Task Title </Text>
+              <Text elipsis={'end'} style={styles.taskTitleText}> Task Title </Text>
             </View>
 
             <View>
-              <Text elipsis={'end'} style={{ fontSize: 18, textTransform: 'capitalize'}}> Task Details </Text>
+              <Text elipsis={'end'} style={styles.taskDescText}> Task Details </Text>
             </View>
 
-            <View style={{marginStart: '70%' , flexDirection: 'row', alignSelf: 'center',marginTop: 8}}>
-              <View style={{ borderRadius: 50, backgroundColor: 'red', height: 15, width: 15, alignSelf: 'center' }} />
+            <View style={styles.labelContainer}>
+              <View style={styles.labelColor}/>
               <Text elipsis={'end'}> Personal </Text>
             </View>
 
@@ -29,5 +29,47 @@ class TaskListItem extends Component{
     );
   }
 }
+
+const styles = StyleSheet.create({
+  root: {
+    height: 100,
+    width: '100%',
+    flexDirection: 'row'
+  },
+  priorityIndicator: {
+    height: '100%',
+    width: '4%',
+    backgroundColor: 'red',
+    marginEnd: 16
+  },
+  taskDataContainer: {
+    marginTop: 18,
+    height: 50,
+    width: '80%'
+  },
+  taskTitleText: {
+    fontWeight: 'bold',
+    fontSize: 22,
+    textTransform: 'capitalize'
+  },
+  taskDescText: {
+    fontWeight: '100',
+    fontSize: 18,
+    textTransform: 'capitalize'
+  },
+  labelContainer: {
+    marginStart: '70%',
+    flexDirection: 'row',
+    alignSelf: 'center',
+    marginTop: 8
+  },
+  labelColor: {
+    borderRadius: 50,
+    backgroundColor: 'red',
+    height: 15,
+    width: 15,
+    alignSelf: 'center'
+  }
+});
 
 export default TaskListItem;
